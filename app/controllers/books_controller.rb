8231c05b7a0f63b2b1721f3890f41e5195e54fce
rina hanyu <gravity0112@gmail.com>
2020-10-17 10:41:20 +0000

    [Fix]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,9 @@
 class BooksController < ApplicationController
-  before_action :ensure_correct_user, only: [:edit]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
     @book_ = Book.new
-    @user = User.find(@book.user_id)
   end
 
   def index
@@ -24,11 +23,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -37,7 +34,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -47,10 +43,10 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def ensure_correct_user
     @book = Book.find(params[:id])
-    unless @book.user_id == current_user.id
+    unless @book.user == current_user
       redirect_to books_path
     end
   end
