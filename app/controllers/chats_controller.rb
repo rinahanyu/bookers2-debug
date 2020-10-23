ff4616cf294684bd8567e5df6a73cc0a113bfaa1
rina hanyu <gravity0112@gmail.com>
2020-10-23 08:10:18 +0000

    [Add]チャット機能非同期通信

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,7 +3,8 @@ class ChatsController < ApplicationController
   def create
     if UserRoom.where(user_id: current_user.id, room_id: params[:chat][:room_id]).present?
       @chat = Chat.create(params.require(:chat).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
-      redirect_to chat_path(@chat)
+      @room = Room.find_by(id: params[:chat][:room_id])
+      @chats = @room.chats
     else
       flash[:alert] = "メッセージに失敗しました。"
     end
@@ -12,11 +13,9 @@ class ChatsController < ApplicationController
   def show
     @room = Room.find_by(id: params[:room_id])
     @user = User.find_by(id: params[:user_id])
-    # @userRoom = UserRoom(user_id: current_user.id, room_id: @room.id)
     if UserRoom.where(user_id: current_user.id, room_id: @room.id).present?
       @chats = @room.chats
       @chat = Chat.new
-      # @userRooms = @room.userRooms
     else
       redirect_back(fallback_location: root_path)
     end
